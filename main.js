d8f82d9ef5a850370c840b583aa88816516a57a0
dikisulung <dikisulung01@gmail.com>
2023-02-28 22:33:53 +0700

    commit pertama

---
+++ main.js
@@ -0,0 +1,14 @@
+// typed js //
+const typed = new Typed('.multiple-text', {
+  strings: [
+    'Process Engineer',
+    'Field Engineer',
+    'Production Supervisor',
+    'Operator Production',
+    'Chemical Education',
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
